439089f6008d7c147bcb75b536402925bd8b53e1
pksz <2000pks@gmail.com>
2020-09-12 15:14:40 +0530

    added background

---
+++ spike dodge sp/Assets/scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float  playerSpeed = 0f ;
+    [SerializeField]  float rotspeed=0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * playerSpeed);
+    }
+}
---
+++ spike dodge sp/Assets/scripts/backgroundrepeat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundrepeat : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float scrollspeed = 1f;
+    Vector3 startPos;
+   
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((new Vector3(0,-1,0)) *scrollspeed*Time.deltaTime);
+
+        
+        if (transform.position.y < -9.98)
+        {
+            transform.position = startPos;
+        }
+        
+    }
+}
